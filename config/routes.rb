2d4b507b36a2f755c15026d7b41aeafd3ef6255a
hxx <845059331@qq.com>
2014-03-21 16:12:44 +0800

    Show articles
    rails g controller articles
    modify the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Blog2::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :articles
 
   # Example resource route with options:
   #   resources :products do
